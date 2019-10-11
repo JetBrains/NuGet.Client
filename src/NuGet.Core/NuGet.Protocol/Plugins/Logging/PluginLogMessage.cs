78022a140db335e6c0e62c12565abea965e3415a
epeshk <evgeny.peshkov@jetbrains.com>
2019-10-11 12:41:41 +0300

    Publicized some internal entities in NuGet.Protocol

--- src/NuGet.Core/NuGet.Protocol/Plugins/Logging/PluginLogMessage.cs
+++ src/NuGet.Core/NuGet.Protocol/Plugins/Logging/PluginLogMessage.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json.Linq;
 
 namespace NuGet.Protocol.Plugins
 {
-    internal abstract class PluginLogMessage : IPluginLogMessage
+    public abstract class PluginLogMessage : IPluginLogMessage
     {
         private static readonly StringEnumConverter _enumConverter = new StringEnumConverter();
 
