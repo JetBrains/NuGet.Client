7d1e1e428d08b2e910d6de3fe5d075b95b8ded3f
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2019-10-11 11:41:41 +0200

    Initial pack of changes for Rider
    suppress several warnings
    
    fix version of Newtonsoft.Json
    
    Fix custom ValidateProjectSpec implementation signature
    
    Make it possible again to pass custom implementations of `IPluginFactory` to the `PluginManager`
    
    Revert protecting the `IPluginFactory` interface and `PluginDiscoverer` constructor
    
    Downgrade NewtonsoftJsonPackageVersion to 13.0.1
    
    Enable strong name signing
    
    Downgrade for net472
    
    Remove leftovers
    
    security-aware NuGetFolderPath.Temp usages
    
    check chmod on each NuGetTempDirectory get
    
    JetBrains.NuGet.Configurations package id
    
    Ignore RS0016;RS0017;CA1051;CA1063;CA2211 solution wide
    
    Turn error to trace
    
    removed CLSCompliant for NuGet.Credentials
    
    PackageId changes
    
    New sign keys
    
    Hacky extension point in FactoryExtensionsV3
    
    added legacy SpecValidationUtility Support
    
    Exposing DependencyGraphRequestProvider
    
    Publish RestoreArgs.GetEffectiveSources
    
    Publish settings members
    
    Enable portable PDB
    
    Respect IgnoreFailedSources in ResolverGather
    
    Disable CLSCompilant
    
    Make PackagesConfigWriter customizable
    
    Publicized some internal entities in NuGet.Protocol

--- src/NuGet.Core/NuGet.Protocol/SourceCacheContext.cs
+++ src/NuGet.Core/NuGet.Protocol/SourceCacheContext.cs
@@ -108,6 +108,12 @@ namespace NuGet.Protocol.Core.Types
                     Interlocked.CompareExchange(ref _generatedTempFolder, newTempFolder, comparand: null);
                 }
 
+                if (RuntimeEnvironmentHelper.IsLinux && !Directory.Exists(_generatedTempFolder))
+                {
+                    // We use it for its side effect - NuGet Temp directory creation with right permissions
+                    NuGetEnvironment.GetFolderPath(NuGetFolderPath.Temp);
+                }
+
                 return _generatedTempFolder;
             }
 
