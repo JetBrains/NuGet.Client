c5bcd8018339ee560251ab7e1c884f74c49d2d27
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2019-10-11 11:41:41 +0200

    Initial pack of changes for Rider
    suppress several warnings
    
    fix version of Newtonsoft.Json
    
    Fix custom ValidateProjectSpec implementation signature
    
    Make it possible again to pass custom implementations of `IPluginFactory` to the `PluginManager`
    
    Revert protecting the `IPluginFactory` interface and `PluginDiscoverer` constructor
    
    Downgrade NewtonsoftJsonPackageVersion to 13.0.1
    
    Enable strong name signing
    
    Downgrade for net472
    
    Remove leftovers
    
    security-aware NuGetFolderPath.Temp usages
    
    check chmod on each NuGetTempDirectory get
    
    JetBrains.NuGet.Configurations package id
    
    Ignore RS0016;RS0017;CA1051;CA1063;CA2211 solution wide
    
    Turn error to trace
    
    removed CLSCompliant for NuGet.Credentials
    
    PackageId changes
    
    New sign keys
    
    Hacky extension point in FactoryExtensionsV3
    
    added legacy SpecValidationUtility Support
    
    Exposing DependencyGraphRequestProvider
    
    Publish RestoreArgs.GetEffectiveSources
    
    Publish settings members
    
    Enable portable PDB
    
    Respect IgnoreFailedSources in ResolverGather
    
    Disable CLSCompilant
    
    Make PackagesConfigWriter customizable
    
    Publicized some internal entities in NuGet.Protocol

--- src/NuGet.Core/NuGet.Common/PathUtil/NuGetEnvironment.cs
+++ src/NuGet.Core/NuGet.Common/PathUtil/NuGetEnvironment.cs
@@ -26,13 +26,12 @@ namespace NuGet.Common
 
         private static readonly Lazy<string> _getHome = new Lazy<string>(() => GetHome());
 
-        private static string _nuGetTempDirectory = null;
+        // private static string _nuGetTempDirectory = null;
 
         private static readonly ConcurrentDictionary<NuGetFolderPath, string> Cache = new ConcurrentDictionary<NuGetFolderPath, string>();
-
         internal static string NuGetTempDirectory
         {
-            get { return _nuGetTempDirectory ??= GetNuGetTempDirectory(); }
+            get { return GetNuGetTempDirectory(); }
         }
 
         internal static IEnvironmentVariableReader EnvironmentVariableReader { get; } = EnvironmentVariableWrapper.Instance;
@@ -56,7 +55,7 @@ namespace NuGet.Common
                     Directory.CreateDirectory(nuGetScratch);
                     if (chmod(nuGetScratch, 0b111_000_000) != 0)   //0b111_000_000 = 700 permissions
                     {
-                        // Another user created a folder pretending to be us! 
+                        // Another user created a folder pretending to be us!
                         var errno = Marshal.GetLastWin32Error(); // fetch the errno before running any other operation
                         throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture,
                             Strings.UnableToSetNuGetTempFolderPermission,
--- src/NuGet.Core/NuGet.Configuration/Settings/ParsedSettingSection.cs
+++ src/NuGet.Core/NuGet.Configuration/Settings/ParsedSettingSection.cs
@@ -7,14 +7,14 @@ using System.Xml.Linq;
 
 namespace NuGet.Configuration
 {
-    internal sealed class ParsedSettingSection : SettingSection
+    public sealed class ParsedSettingSection : SettingSection
     {
-        internal ParsedSettingSection(string name, XElement element, SettingsFile origin)
+        public ParsedSettingSection(string name, XElement element, SettingsFile origin)
             : base(name, element, origin)
         {
         }
 
-        internal ParsedSettingSection(string name, params SettingItem[] children)
+        public ParsedSettingSection(string name, params SettingItem[] children)
             : base(name, attributes: null, children: new HashSet<SettingItem>(children))
         {
             foreach (var child in Children)
