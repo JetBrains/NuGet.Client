01656c9659d86f1740e9af0aaf4e34c28834530d
Mark Tkachenko <mark.tkachenko@jetbrains.com>
2020-09-20 15:14:14 +0300

    Warnings disable

--- src/NuGet.Core/NuGet.Protocol/HttpSource/HttpSourceAuthenticationHandler.cs
+++ src/NuGet.Core/NuGet.Protocol/HttpSource/HttpSourceAuthenticationHandler.cs
@@ -96,21 +96,12 @@ namespace NuGet.Protocol
                 if (response.StatusCode == HttpStatusCode.Unauthorized ||
                     (configuration.PromptOn403 && response.StatusCode == HttpStatusCode.Forbidden))
                 {
-                    List<Stopwatch> stopwatches = null;
-
-#if USE_HTTPREQUESTMESSAGE_OPTIONS
-                    if (request.Options.TryGetValue(
-                        new HttpRequestOptionsKey<List<Stopwatch>>(HttpRetryHandler.StopwatchPropertyName),
-                        out stopwatches))
-                    {
-#else
-                    // stop ignoring CS0618 when fixing https://github.com/NuGet/Home/issues/9981
-#pragma warning disable CS0618
+                    IList<Stopwatch> stopwatches = null;
+#pragma warning disable
                     if (request.Properties.TryGetValue(HttpRetryHandler.StopwatchPropertyName, out var value))
                     {
                         stopwatches = value as List<Stopwatch>;
 #pragma warning restore CS0618
-#endif
                         if (stopwatches != null)
                         {
                             foreach (var stopwatch in stopwatches)
